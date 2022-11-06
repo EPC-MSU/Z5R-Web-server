2a7295942dffc6b24e4d2abfbd8694f52c7d1a31
Sergey Zapunidi <zapunidy@gmail.com>
2022-11-06 15:25:41 +0300

    Made html forms support different controllers

--- src/z5r/web_page.py
+++ src/z5r/web_page.py
@@ -1,95 +1,117 @@
 def per_controller_page(sn):
+    # Heading
     answer = '<h2 style="text-align: center;">Controller SN{}</h1>'.format(sn)
+
+    # Forms outside the table
     forms = ['open_door', 'set_mode', 'set_door_params', 'add_cards', 'del_cards', 'clear_cards']
     for form in forms:
         answer += """
-<form action="/" id="{}" method="get">
+<form action="/" id="{}_{}" method="get">
 <input type="hidden" name="sn" value="{}">
-</form>""".format(form, sn)
-    return answer + """
+</form>""".format(form, sn, sn)
+
+    # Table start
+    answer += """
 <table style="border-collapse: collapse; width: 100%; height: 108px;" border="1">
-<tbody>
+<tbody>"""
+
+    answer += f"""
 <tr style="height: 18px;">
 <td style="width: 33.3333%; height: 18px;">
-<button name="action" type="submit" value="open_door" form="open_door">Open door</button>
+<button name="action" type="submit" value="{forms[0]}" form="{forms[0]}_{sn}">Open door</button>
 </td>
 <td style="width: 33.3333%; height: 18px;">
-<label for="open_door_direction">Direction:</label>
-<input type="text" name="open_door_direction" value="0" form="open_door">
+<label for="{forms[0]}_direction">Direction:</label>
+<input type="text" name="{forms[0]}_direction" value="0" form="{forms[0]}_{sn}">
 </td>
 <td style="width: 33.3333%; height: 18px;">
 Opens the door. Direction 0 is for entrance. Direction 1 is for exit.
 </td>
-</tr style="height: 18px;">
+</tr>"""
+
+    answer += f"""
+<tr style="height: 18px;">
 <td style="width: 33.3333%; height: 18px;">
-<button name="action" type="submit" value="set_mode" form="set_mode">Set mode</button>
+<button name="action" type="submit" value="{forms[1]}" form="{forms[1]}_{sn}">Set mode</button>
 </td>
 <td style="width: 33.3333%; height: 18px;">
-<label for="set_mode_mode">Mode:</label>
-<input type="text" name="set_mode_mode" value="0" form="set_mode">
+<label for="{forms[1]}_mode">Mode:</label>
+<input type="text" name="{forms[1]}_mode" value="0" form="{forms[1]}_{sn}">
 </td>
 <td style="width: 33.3333%; height: 18px;">
 Sets controller mode: 0 - normal, 1 - block, 2 - free passage, 3 - waiting for free passage.
 </td>
-</tr>
+</tr>"""
+
+    answer += f"""
 <tr style="height: 18px;">
 <td style="width: 33.3333%; height: 18px;">
-<button name="action" type="submit" value="set_door_params" form="set_door_params">Set door params</button>
+<button name="action" type="submit" value="{forms[2]}" form="{forms[2]}_{sn}">Set door params</button>
 </td>
 <td style="width: 33.3333%; height: 18px;">
-<label for="set_door_params_open">Open:</label>
-<input type="text" name="set_door_params_open" value="30" form="set_door_params"><br>
-<label for="set_door_params_open_control">Open control:</label>
-<input type="text" name="set_door_params_open_control" value="50" form="set_door_params"><br>
-<label for="set_door_params_close_control">Close control:</label>
-<input type="text" name="set_door_params_close_control" value="50" form="set_door_params">
+<label for="{forms[2]}_open">Open:</label>
+<input type="text" name="{forms[2]}_open" value="30" form="{forms[2]}_{sn}"><br>
+<label for="{forms[2]}_open_control">Open control:</label>
+<input type="text" name="{forms[2]}_open_control" value="50" form="{forms[2]}_{sn}"><br>
+<label for="{forms[2]}_close_control">Close control:</label>
+<input type="text" name="{forms[2]}_close_control" value="50" form="{forms[2]}_{sn}">
 </td>
 <td style="width: 33.3333%; height: 18px;">
 Sets the time for opening and closing of the door. Open is time for opening door signal [1/10s].
 Open control is time of control for opened door [1/10s]). Close control is time of control for closing door [1/10s].
 </td>
-</tr>
+</tr>"""
+
+    answer += f"""
 <tr style="height: 18px;">
 <td style="width: 33.3333%; height: 18px;">
-<button name="action" type="submit" value="add_cards" form="add_cards">Add cards</button>
+<button name="action" type="submit" value="{forms[3]}" form="{forms[3]}_{sn}">Add cards</button>
 </td>
 <td style="width: 33.3333%; height: 18px;">
-<label for="add_cards_card">Card number in HEX:</label>
-<input type="text" name="add_cards_card" value="123456789ABC" form="add_cards"><br>
-<label for="add_cards_flags">Flags:</label>
-<input type="text" name="add_cards_flags" value="0" form="add_cards"><br>
-<label for="add_cards_tz">Time zone:</label>
-<input type="text" name="add_cards_tz" value="255" form="add_cards">
+<label for="{forms[3]}_card">Card number in HEX:</label>
+<input type="text" name="{forms[3]}_card" value="123456789ABC" form="{forms[3]}_{sn}"><br>
+<label for="{forms[3]}_flags">Flags:</label>
+<input type="text" name="{forms[3]}_flags" value="0" form="{forms[3]}_{sn}"><br>
+<label for="{forms[3]}_tz">Time zone:</label>
+<input type="text" name="{forms[3]}_tz" value="255" form="{forms[3]}_{sn}">
 </td>
 <td style="width: 33.3333%; height: 18px;">
 Insert manual here
 </td>
-</tr>
+</tr>"""
+
+    answer += f"""
 <tr style="height: 18px;">
 <td style="width: 33.3333%; height: 18px;">
-<button name="action" type="submit" value="del_cards" form="del_cards">Delete cards</button>
+<button name="action" type="submit" value="{forms[4]}" form="{forms[4]}_{sn}">Delete cards</button>
 </td>
 <td style="width: 33.3333%; height: 18px;">
-<label for="del_cards_card">Card number in HEX:</label>
-<input type="text" name="del_cards_card" value="123456789ABC" form="del_cards">
+<label for="{forms[4]}_card">Card number in HEX:</label>
+<input type="text" name="{forms[4]}_card" value="123456789ABC" form="{forms[4]}_{sn}">
 </td>
 <td style="width: 33.3333%; height: 18px;">
 Insert manual here
 </td>
-</tr>
+</tr>"""
+
+    answer += f"""
 <tr style="height: 18px;">
 <td style="width: 33.3333%; height: 18px;">
-<button name="action" type="submit" value="clear_cards" form="clear_cards">Clear cards</button>
+<button name="action" type="submit" value="{forms[5]}" form="{forms[5]}_{sn}">Clear cards</button>
 </td>
 <td style="width: 33.3333%; height: 18px;">
 </td>
 <td style="width: 33.3333%; height: 18px;">
 Insert manual here
 </td>
-</tr>
+</tr>"""
+
+    # Table end
+    answer += """
 </tbody>
 </table>
 """
+    return answer
 
 
 def get_page(controllers_dict):
@@ -107,6 +129,5 @@ def get_page(controllers_dict):
     answer = head
     for controller in controllers_dict:
         answer += per_controller_page(controller.sn)
-    answer += per_controller_page(0)
     answer += tail
     return answer
