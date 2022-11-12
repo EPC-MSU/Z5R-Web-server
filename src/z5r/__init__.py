041228995707b7398e21fc888d06e93a25a02ffb
Sergey Zapunidi <zapunidy@gmail.com>
2022-11-12 21:18:29 +0300

    Split control and attendance page

--- src/z5r/__init__.py
+++ src/z5r/__init__.py
@@ -1,2 +1,3 @@
 from .z5r_web import Z5RWebController  # noqa
-from .control_page import get_page, get_attendance_page, action_handler  # noqa
+from .control_page import get_page, action_handler  # noqa
+from .attendance_page import get_attendance_page  # noqa
---
+++ src/z5r/attendance_page.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+from datetime import timedelta
+import sqlite3
+
+
+DAY_TO_SHOW = 5
+
+
+def _get_events_by_date(databases, start_datetime, end_datetime, card_filter=False):
+    if card_filter:
+        sql_flt = ' AND card != "000000000000"'
+    else:
+        sql_flt = ''
+    res_cat = list()
+    for dbname in databases:
+        con = sqlite3.connect(dbname)
+        cur = con.cursor()
+        cur.execute(
+            'SELECT time, card, event_name FROM events WHERE time > {} AND time < {}{} ORDER BY time'.format(
+                int(start_datetime.timestamp()), int(end_datetime.timestamp()), sql_flt
+            ))
+        res = cur.fetchall()
+        if len(res) == 0:
+            continue
+        res_cat += res
+
+    return res_cat
+
+
+def get_attendance_page(controllers_dict):
+    head = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <title>Z5R attendance</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <style>
+    .collapsible {
+      background-color: #777;
+      color: white;
+      cursor: pointer;
+      padding: 5px;
+      width: 100%;
+      border: 2px solid #888;
+      text-align: left;
+      outline: none;
+      font-size: 15px;
+    }
+
+    .active, .collapsible:hover {
+      background-color: #555;
+    }
+
+    .content {
+      padding: 0 5px;
+      display: none;
+      overflow: hidden;
+      background-color: #f1f1f1;
+    }
+
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+
+    div {
+      padding: 5px;
+    }
+    </style>
+    </head>
+    <body>
+    <h1 style="text-align: center;">Z5R-Web general attendance</h1>
+    """
+    tail = """
+
+    <script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+    </script>
+
+    </body>
+    </html>
+    """
+    answer = head
+
+    # Start event collapsible view
+    start = datetime.now().date() - timedelta(DAY_TO_SHOW)  # The end of this day
+    days = [datetime(start.year, start.month, start.day) + timedelta(i) for i in range(0, DAY_TO_SHOW + 2)]
+    databases = ['service_data/{}_events.db'.format(sn) for sn in controllers_dict]
+    for day in range(0, DAY_TO_SHOW + 1):
+        res = _get_events_by_date(databases, days[day], days[day + 1], card_filter=True)
+        if len(res) == 0:
+            continue
+
+        def split_first_last(events_list):
+            card_events = dict()
+            for event in events_list:  # event is [time, card, event_name]
+                if event[1] not in card_events:  # New card in list
+                    card_events[event[1]] = [event[0], event[0]]  # Write it as start and end
+                else:
+                    if event[0] < card_events[event[1]][0]:  # If time of event is earlier than start
+                        card_events[event[1]][0] = event[0]  # Write it as start time
+                    if event[0] > card_events[event[1]][1]:  # If time of event is later than end
+                        card_events[event[1]][1] = event[0]  # Write it as end time
+            return card_events
+
+        def em_marine(card_hex):
+            if len(card_hex) != 12:
+                return 'N/A'
+            if card_hex[0:6] != '000000':
+                return 'N/A'
+            return '{},{:05}'.format(int(card_hex[6:8], base=16), int(card_hex[8:12], base=16))
+
+        work_periods = split_first_last(res)
+        answer += '<button type="button" class="collapsible">{}</button>'.format(days[day].strftime('%d %b'))
+        answer += '<table class="content">'
+        answer += '<tr><td>Card HEX</td><td>Card Em-Marine</td><td>First event</td><td>Last event</td></tr>'
+        for card in work_periods:
+            answer += '<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'.format(
+                card,
+                em_marine(card),
+                datetime.fromtimestamp(int(work_periods[card][0])).strftime('%H:%M:%S'),
+                datetime.fromtimestamp(int(work_periods[card][1])).strftime('%H:%M:%S')
+            )
+        answer += '</table>'
+
+    answer += tail
+    return answer
