ef06c9ca5c968d61082238aa6b361224e407b0cd
Sergey Zapunidi <zapunidy@gmail.com>
2022-11-12 21:12:34 +0300

    Renamed web page handler

--- src/z5r/__init__.py
+++ src/z5r/__init__.py
@@ -1,2 +1,2 @@
 from .z5r_web import Z5RWebController  # noqa
-from .web_page import get_page, get_attendance_page, action_handler  # noqa
+from .control_page import get_page, get_attendance_page, action_handler  # noqa
