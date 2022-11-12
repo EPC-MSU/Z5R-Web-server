32106abb470f385d857e11b6e0b6bbccd38215a8
Sergey Zapunidi <zapunidy@gmail.com>
2022-11-12 22:47:51 +0300

    Fixed users page

--- src/z5r/users_page.py
+++ src/z5r/users_page.py
@@ -1,6 +1,74 @@
-def users_handler():
+def users_handler(query, controllers_dict):
     pass
 
 
-def get_users_page():
-    pass
+def get_users_page(controllers_dict):
+    head = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <title>Z5R control</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <style>
+    .collapsible {
+      background-color: #777;
+      color: white;
+      cursor: pointer;
+      padding: 5px;
+      width: 100%;
+      border: 2px solid #888;
+      text-align: left;
+      outline: none;
+      font-size: 15px;
+    }
+
+    .active, .collapsible:hover {
+      background-color: #555;
+    }
+
+    .content {
+      padding: 0 5px;
+      display: none;
+      overflow: hidden;
+      background-color: #f1f1f1;
+    }
+
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+
+    div {
+      padding: 5px;
+    }
+    </style>
+    </head>
+    <body>
+    <h1 style="text-align: center;">Z5R-Web users page</h1>
+    """
+    tail = """
+
+    <script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+    </script>
+
+    </body>
+    </html>
+    """
+    answer = head
+    answer += tail
+    return answer
